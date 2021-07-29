69289e113fab5e7a862eede299c1282aaf97cef0
Maikoncarlos <maikonsaturno@gmail.com>
2021-07-29 14:06:40 -0300

    commit inicial

---
+++ src/main/java/Maikoncarlos/com/github/maikonrestapi/MaikonRestApiApplication.java
@@ -0,0 +1,16 @@
+package Maikoncarlos.com.github.maikonrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaikonRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaikonRestApiApplication.class, args);
+
+		System.out.println("Hello Words");
+	}
+
+
+}
---
+++ src/test/java/Maikoncarlos/com/github/maikonrestapi/MaikonRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package Maikoncarlos.com.github.maikonrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaikonRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
